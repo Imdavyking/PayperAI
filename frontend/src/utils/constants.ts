e4b91fc5ce559340896c72280471d0f22ff2ef4c
imdavyking <yungdynamic53@gmail.com>
2026-01-08 03:57:48 +0100

    chore: updated

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const tokens = [];
 export const MEME_FACTORY = import.meta.env.VITE_MEME_FACTORY;
 export const MODULE_NAME = "PayPerAI";
-export const SERVER_URL = "http://localhost:3000";
+export const SERVER_URL =
+  import.meta.env.VITE_SERVER_URL || "http://localhost:8080";
